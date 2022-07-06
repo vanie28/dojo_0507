a1220f8a5b49a6541617f090e5564433a6a806cc
vanie28 <sylvanie.giraudet@sncf.fr>
2022-07-06 15:00:47 +0200

    exo final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Tab1 from "./components/Tab1";
+import Tab2 from "./components/Tab2";
+
+// function ShowQuotes(props) {
+//   useEffect(() => {
+//     return () => {
+//       console.log("show A se demonte");
+//       props.handleReset();
+//     };
+//   }, [props.show]);
+//   return (
+//     <div className="showtab">
+//       {props.quotes.length > 0 ? (
+//         <>
+//           <h3></h3>
+//           <img></img>
+//         </>
+//       ) : null}
+//     </div>
+//   );
+// }
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [show, setShow] = useState(true);
+
+  const handleReset = () => {
+    setQuotes([]);
+  };
+
+  const getSomething = async () => {
+    const promise = await fetch(
+      "https://simpsons-quotes-api.herokuapp.com/quotes?count=1"
+    );
+    const result = await promise.json();
+    // console.log("@@@", promise);
+    // console.log("result:", result);
+    setQuotes(result);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => setShow(!show)}>Change Tab</button>
+      <div className="App">
+        <div className="left">
+          {show ? (
+            <>
+              <button onClick={() => getSomething()}>Get quotes</button>
+              <Tab1 show={show} handleReset={handleReset} quotes={quotes} />
+            </>
+          ) : null}
+        </div>
+        <div className="right">
+          {show ? null : (
+            <>
+              <button onClick={() => getSomething()}>Get quotes</button>
+                <Tab2 show={show} handleReset={handleReset} quotes={quotes}/>
+            </>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
-
 export default App;
---
+++ src/components/Tab2.jsx
@@ -0,0 +1,23 @@
+import {useEffect} from 'react';
+
+function Tab2(props) {
+  useEffect(() => {
+    return () => {
+      console.log("show A se demonte");
+      props.handleReset();
+    };
+  },[props.show]);
+  return (
+    <div className="showtab">
+      {props.quotes.length > 0 ? (
+        <>
+          <h1>{props.quotes[0].character}</h1>
+          <img src={props.quotes[0].image} alt=""></img>
+          <h3>{props.quotes[0].quote}</h3>
+        </>
+      ) : null}
+    </div>
+  );
+}
+
+export default Tab2;
